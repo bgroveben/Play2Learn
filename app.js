bb9141d695d389a3fed74950fdf091f9687c1b17
bgroveben <bgroveben@gmail.com>
2022-02-24 23:00:44 -0800

    Clean up content and files

--- app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const {check, validationResult} = require('express-validator');
-const app = express();
-
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
